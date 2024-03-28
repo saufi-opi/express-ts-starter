b1de94208461eda5e4e13c43768bdd696710d59a
Saufi <saufi1115@gmail.com>
2024-03-28 23:25:10 +0800

    feat: requireAuth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 import { Account } from '../interfaces/account.interface'
+import { createError } from '../utils/error'
 
 declare module 'express' {
   interface Request {
@@ -11,4 +12,11 @@ declare module 'express' {
 // TODO: auth middleware
 export function authMiddleware(request: Request, response: Response, next: NextFunction) {
   next()
+}
+
+export function requireAuth(request: Request, _response: Response, next: NextFunction) {
+  if (request.role !== 'guest') {
+    return next()
+  }
+  throw createError(401, 'error.no-permission')
 }
