1c2cc81cadce982e4b10e5c95165e2fd83048073
Saufi <saufi1115@gmail.com>
2024-03-30 17:02:26 +0800

    refactor: add logger

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,15 +1,14 @@
 import { Request, Response, NextFunction } from 'express'
 import { ApiError } from '../interfaces/api-error.interface'
 import { ErrorResponse } from '../interfaces/response.interface'
+import logger from '../utils/logger'
 
 export function errorMiddleware(error: ApiError, _request: Request, response: Response<ErrorResponse>, next: NextFunction) {
   try {
     error.statusCode ??= 500
     error.message ??= 'default-error-message'
 
-    if (response.headersSent) {
-      return next(error)
-    }
+    logger.error({ statusCode: error.statusCode, message: error.message })
     response.status(error.statusCode).json({ statusCode: error.statusCode, success: false, message: error.message })
   } catch (error) {
     next(error)
