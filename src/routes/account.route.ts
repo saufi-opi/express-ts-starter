6902e0a69db2d9b8d543c5d29ea2d79b8ab947e0
Saufi <saufi1115@gmail.com>
2024-03-30 17:04:23 +0800

    feat: complete account route

--- src/routes/account.route.ts
+++ src/routes/account.route.ts
@@ -14,7 +14,8 @@ class AccountRoute implements Routes {
   private initializeRoutes() {
     this.router.get(this.path, this.ctr.getAccounts)
     this.router.post(this.path, this.ctr.createAccount)
-    this.router.put(this.path, this.ctr.updateAccount)
+    this.router.put(`${this.path}/:id`, this.ctr.updateAccount)
+    this.router.delete(`${this.path}/:id`, this.ctr.deleteAccount)
   }
 }
 
