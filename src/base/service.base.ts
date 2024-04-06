bc16688ba761007dfd9d99be7571997d62a7c763
Saufi <saufi1115@gmail.com>
2024-04-07 02:22:25 +0800

    refactor: use mongoose queryoptions type

--- src/base/service.base.ts
+++ src/base/service.base.ts
@@ -1,69 +1,103 @@
-import { ClientSession, FilterQuery, Model } from 'mongoose'
+import { AggregateOptions, ClientSession, FilterQuery, Model, PipelineStage, QueryOptions } from 'mongoose'
 import { createError, createErrorFactory } from '../utils/error'
 import { appendDoc } from '../utils/database'
+import { Resources } from '../interfaces/response.interface'
 
 export interface ServiceOptions {
-  session?: ClientSession
-  projection?: Record<string, boolean>
+  dbName: string
+  primaryKey?: string
+}
+
+export interface SearchQueryOptions {
+  search?: string | Record<string, unknown>
+  filter?: string | Record<string, unknown>
+  sort?: string
+  page?: number
+  pageSize?: number
+}
+
+export interface ExtendedSearchQueryOptions extends SearchQueryOptions {
+  account?: string
 }
 
 export class BaseService<T extends { id: string }> {
   protected model: Model<T>
   public error: typeof createError
+  public primaryKey: string
 
-  constructor(model: Model<T>, dbName: string) {
+  constructor(model: Model<T>, options: ServiceOptions) {
+    options.primaryKey ??= 'id'
+
     this.model = model
-    this.error = createErrorFactory(dbName)
+    this.error = createErrorFactory(options.dbName)
+    this.primaryKey = options.primaryKey
   }
 
-  public async search(filter: FilterQuery<T> = {}): Promise<T[]> {
-    const items = await this.model.find(filter, { password: false })
+  public aggregationPipeline(options: ExtendedSearchQueryOptions): PipelineStage[] {
+    // TODO: make filtering, pagination, sort
+    // TODO: make a pipeline to lookup from permission collection and if doesnt have permission with read FLAG, remove from returning to user
+    const pipeline: PipelineStage[] = []
+    return pipeline
+  }
+
+  public async search(query: ExtendedSearchQueryOptions = {}, options?: AggregateOptions): Promise<T[]> {
+    options ??= {}
+    const items = await this.model.aggregate(this.aggregationPipeline(query), options)
     return items
   }
 
-  public async insertOne(docs: Partial<T>, options?: ServiceOptions): Promise<T | null> {
+  public async insertOne(docs: Partial<T>, options?: QueryOptions<T>): Promise<T | null> {
+    options ??= {}
+    options.upsert = true
+    options.returnDocument = 'after'
     docs = appendDoc(docs)
-    const item = await this.model.findOneAndReplace({ id: docs.id }, docs, {
-      ...options,
-      upsert: true,
-      returnDocument: 'after'
-    })
-
+    const item = await this.model.findOneAndReplace({ id: docs.id }, docs, options)
     return item
   }
 
-  public async findOne(filter: FilterQuery<T>, options?: ServiceOptions): Promise<T | null> {
-    const item = await this.model.findOne(filter, options, options)
+  public async findOne(filter: FilterQuery<T>, options?: QueryOptions<T>): Promise<T | null> {
+    options ??= {}
+    const item = await this.model.findOne(filter, {}, options)
     return item
   }
 
-  public async updateOne(filter: FilterQuery<T>, docs: Partial<T>, options?: ServiceOptions): Promise<T | null> {
-    const item = await this.model.findOneAndUpdate(filter, docs, {
-      ...options,
-      returnDocument: 'after'
-    })
+  public async findById(id: string, options?: QueryOptions<T>): Promise<T | null> {
+    const item = await this.model.findOne({ id }, {}, options)
     return item
   }
 
-  public async updateMany(filter: FilterQuery<T>, docs: Partial<T>, options?: ServiceOptions): Promise<T[]> {
+  public async updateOne(filter: FilterQuery<T>, docs: Partial<T>, options?: QueryOptions<T>): Promise<T | null> {
+    options ??= {}
+    options.returnDocument = 'after'
+    const item = await this.model.findOneAndUpdate(filter, docs, options)
+    return item
+  }
+
+  public async updateMany(filter: FilterQuery<T>, docs: Partial<T>, options?: QueryOptions<T>): Promise<T[]> {
+    options ??= {}
     const items = await this.model.find(filter, {}, options)
-    await this.model.updateMany(filter, docs, options)
-    const results = await this.model.find({ id: { $in: items.map((o) => o.id) } }, options, options)
+    await this.model.updateMany(filter, docs, { session: options?.session })
+    const results = await this.model.find({ id: { $in: items.map((o) => o.id) } }, {}, options)
     return results
   }
 
-  public async deleteOne(filter: FilterQuery<T>, options?: ServiceOptions): Promise<T | null> {
-    const item = await this.model.findOneAndDelete(filter, {
-      ...options,
-      returnDocument: 'after'
-    })
+  public async deleteOne(filter: FilterQuery<T>, options?: QueryOptions<T>): Promise<T | null> {
+    options ??= {}
+    options.returnDocument = 'after'
+    const item = await this.model.findOneAndDelete(filter, options)
     return item
   }
 
-  public async deleteMany(filter: FilterQuery<T>, options?: ServiceOptions): Promise<T[]> {
+  public async deleteMany(filter: FilterQuery<T>, options?: QueryOptions<T>): Promise<T[]> {
+    options ??= {}
     const items = await this.model.find(filter, options, options)
-    await this.model.deleteMany(filter, options)
+    await this.model.deleteMany(filter, { session: options?.session })
     return items
+  }
+
+  public async resources(docs: T, session: ClientSession) {
+    const resources: Resources = {}
+    return resources
   }
 }
 
