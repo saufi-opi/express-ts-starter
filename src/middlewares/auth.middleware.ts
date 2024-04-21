809ed107b8493af3ea09fc0a68dc496b6df93f16
Saufi <saufi1115@gmail.com>
2024-04-21 15:26:16 +0800

    feat: finish up auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,16 +1,35 @@
 import { Request, Response, NextFunction } from 'express'
-import { Account } from '../interfaces/account.interface'
+import { Account, AccountModel } from '../routes/account/account.model'
 import { createError } from '../utils/error'
+import AccountService from '../routes/account/account.service'
+import databaseNames from '../databases/database.names'
+import { findTokenFromHeader, getAccountIdFromToken } from '../routes/authenticate/authenticate.helper'
+import { AuthenticateService } from '../routes/authenticate/authenticate.service'
+import { AuthenticateModel } from '../routes/authenticate/authenticate.model'
 
 declare module 'express' {
   interface Request {
-    user: Account
+    account: Account
     role: string
   }
 }
 
-// TODO: auth middleware
-export function authMiddleware(request: Request, response: Response, next: NextFunction) {
+export async function authMiddleware(request: Request, _response: Response, next: NextFunction) {
+  const accountService = new AccountService(AccountModel, { dbName: databaseNames.account })
+  const authService = new AuthenticateService(AuthenticateModel, { dbName: databaseNames.authenticate })
+
+  const token = findTokenFromHeader(request)
+  if (token) {
+    const accountId = getAccountIdFromToken(token)
+    const authRecord = await authService.findOne({ account: accountId, token })
+    if (!authRecord) {
+      request.role = 'guest'
+    } else {
+      const account = await accountService.findOne({ id: accountId })
+      request.account = account
+      request.role = account.role
+    }
+  }
   next()
 }
 
