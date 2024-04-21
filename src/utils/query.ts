de11203b657cd48561c7c859eec90aaf6cf3f60d
Saufi <saufi1115@gmail.com>
2024-04-21 23:53:40 +0800

    refactor: request.query.role

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -2,6 +2,7 @@ import { Request } from 'express'
 
 export function retrieveQuery(request: Request) {
   const query = request.query
-  query.account = request.account.id
+  query.account = request.account?.id
+  query.role = request.account?.role ?? 'guest'
   return query
 }
